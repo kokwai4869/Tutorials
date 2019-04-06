953c7f7c603841fbef56aa67dbd864bbea6e5db9
kokwai4869 <kokwai@gmail.com>
2019-04-06 20:04:56 +0900

    Add new methods

--- MethodsString.py
+++ MethodsString.py
@@ -6,11 +6,9 @@ Created on Fri Apr  5 17:36:21 2019
 """
 from itertools import permutations
 
-import DataProcessing
-
 def get_longest_palindrome(data):
     n_test = data[0]
-    data = data[1:]
+    data = data[1]
     string_list = []
     for i in range(n_test):
         string_list.append(data[i])
@@ -27,30 +25,58 @@ def get_longest_palindrome(data):
 
 def reverse_string_words(data):
     n_test = data[0]
-    data = data[1:]
+    data = data[1]
     results = []
     for i in range(n_test):
-        results.append(".".join(reversed(data[i][0].split("."))))
+        results.append(".".join(reversed(data[i].split("."))))
     return results
 
 def permutation_of_given_strings(data):
     n_test = data[0]
-    data = data[1:]
+    data = data[1]
     results = []
     for i in range(n_test):
         results.append(" ".join(list(map(lambda x: "".join(x),
-                                         permutations(data[i][0])))))
+                                         permutations(data[i])))))
     return results
 
 def recursively_remove_all_adjacent_duplicates(data):
     n_test = data[0]
-    data = data[1:]
+    data = data[1]
     results = []
     for i in range(n_test):
         sentence = []
-        for index_c in range(len(data[i][0])):
-            if data[i][0][index_c] != data[i][0][(index_c+1)%len(data[i][0])] \
-            and data[i][0][index_c] != data[i][0][(index_c-1)%len(data[i][0])]:
-                sentence.append(data[i][0][index_c])                
+        for index_c in range(len(data[i])):
+            if data[i][index_c] != data[i][(index_c+1)%len(data[i])] \
+            and data[i][index_c] != data[i][(index_c-1)%len(data[i])]:
+                sentence.append(data[i][index_c])                
         results.append("".join(sentence))
     return results
+
+def roman_number_to_integer(data):
+    n_test = data[0]
+    data = data[1]
+    results = []
+    dictionary = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+    reverse = {value:key for key, value in dictionary.items()}
+    for i in range(n_test):
+        string_list = list(*[iter(data[i])])
+        string_length = len(string_list)
+        # Set the highest roman value as the middle character and get its index
+        mid_highest_index = string_list.index(reverse[max([dictionary[x] for
+                                                           x in string_list])])
+        count = 0
+        # We compare if the index is more than mid than we add otherwise minus
+        for i in range(string_length):
+            if i > mid_highest_index:
+                count += dictionary[string_list[i]]
+            elif i == mid_highest_index:
+                count += dictionary[string_list[i]]
+            else:
+                count -= dictionary[string_list[i]]
+        results.append(count)
+    return results
+                    
+                    
+            
+    
